85c18d5fb628d067d8f8c3911ff940b4612e1ab0
Drabants <drabants@gmail.com>
2017-04-16 12:02:52 -0700

    Update solution.rb

--- Learning-Ruby/Sum-Multiples/solution.rb
+++ Learning-Ruby/Sum-Multiples/solution.rb
@@ -1,5 +1,12 @@
-#Method sums up the multiples of the first number given within the
-#range of the second and third number given
+#Iterates through all numbers 1 to 1000 to calculate the sum of multiples 3 or 5
+sum =0
+for i in 1...1000
+	if (i%3 == 0 || i%5 ==0)
+		sum += i
+	end
+end
+puts sum
+#This method sums up the numbers that are multiples of mult within the range first and last. It is then #called three times. Twice to add the sums of 3 and 5 and once to subtract the duplicates created by #multiples of 15. This reduces the amount of numbers iterated through by 40.1%
 def sumMultiplesInRange(mult, first, last)
 	i = first
 	sum = 0
@@ -9,15 +16,3 @@ def sumMultiplesInRange(mult, first, last)
 	end
 	return sum
 end
-#Reduces the amount of numbers iterated through by 40.2%
-puts sumMultiplesInRange(3, 1, 1000) + sumMultiplesInRange(5,1,1000) - sumMultiplesInRange(15,1,1000)
-
-#Iterates through all numbers 1 to 1000 to calulate the sum
-#of multiples 3 or 5
-sum =0
-for i in 1...1000
-	if (i%3 == 0 || i%5 ==0)
-		sum += i
-	end
-end
-puts sum
