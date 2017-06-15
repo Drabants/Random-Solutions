e5354da83e456b358738bb45ea867b288922c716
Drabants <drabants@gmail.com>
2017-06-15 15:35:21 -0700

    Create solution.cpp

---
+++ Cplusplus Practice/Remove-Element/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int len = nums.size();
+        for(int i =0; i<len; i++){
+            if(nums[i] == val){
+                nums.erase(nums.begin()+i);
+                len = nums.size();
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
