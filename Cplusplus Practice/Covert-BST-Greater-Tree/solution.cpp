e3b312ee45aedcf2d92bb3cada263e83c0ec0ce6
Drabants <drabants@gmail.com>
2017-07-04 15:16:22 -0700

    Create solution.cpp

---
+++ Cplusplus Practice/Covert-BST-Greater-Tree/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    int sum=0;
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        if(!root){
+            return root;
+        }
+        else{
+            if (root->right){
+                convertBST(root->right);
+            }
+            root->val = (sum+= root->val);
+            if (root->left){
+                convertBST(root->left);                
+            }
+        }
+        return root;
+    }
+};
